e9fe7eb5c02506c04e74f1ac667cd5b1f6962bee
Michael Hudson <mnkotagu@gmail.com>
2023-06-25 05:18:34 +0300

    Implement 100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,17 @@
+# Puppet file to set up your client SSH configuration file so 
+# connection to a server can be done without typing a password
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
+}
