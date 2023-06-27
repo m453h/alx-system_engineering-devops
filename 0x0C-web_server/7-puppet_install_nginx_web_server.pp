a8d22756d69374d741b991a62ef20f43ec42c1de
Michael Hudson <mnkotagu@gmail.com>
2023-06-28 00:29:12 +0300

    Implement 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,3 +3,13 @@
 package {'nginx':
   ensure => 'present',
 }
+
+exec {'install_nginx_web_server':
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  provider => shell,
+}
+
+exec {'set_hello_world_page':
+  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+  provider => shell,
+}
