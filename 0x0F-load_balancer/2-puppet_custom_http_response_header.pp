7d8ce7caade21d5bd797ef546fcf55810790037f
Michael Hudson <mnkotagu@gmail.com>
2023-07-04 02:24:33 +0300

    Fix lint errors

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,10 +1,10 @@
 # Installs Nginx web server using Puppet
-
+$my_host = $facts['networking']['hostname']
 package {'nginx':
   ensure => 'present',
 }
 ->exec {'redirect_page':
-  command  => 'sudo sed -i "12i add_header X-Served-By \"${hostname}\";" /etc/nginx/sites-available/default',
+  command  => "sudo sed -i \"12i add_header X-Served-By '${my_host}';\" /etc/nginx/sites-available/default",
   provider => shell,
 }
 ->exec {'run':
