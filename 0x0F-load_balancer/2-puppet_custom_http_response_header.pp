63844d8cc94e567f72e03242ecca1fcf8b39fd17
Michael Hudson <mnkotagu@gmail.com>
2023-07-04 01:18:02 +0300

    Remove comments used when testing implementation

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,13 +1,13 @@
 # Installs Nginx web server using Puppet
 
-#package {'nginx':
-#  ensure => 'present',
-#}
+package {'nginx':
+  ensure => 'present',
+}
 
-#exec {'install_nginx_web_server':
-#  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
-#  provider => shell,
-#}
+exec {'install_nginx_web_server':
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  provider => shell,
+}
 
 exec {'set_hello_world_page':
   command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
