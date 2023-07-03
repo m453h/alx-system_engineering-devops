49821ba08aba485310f1a60545ebbada21acef5f
Michael Hudson <mnkotagu@gmail.com>
2023-07-04 02:20:18 +0300

    Attempt very minimal solution

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,23 +3,11 @@
 package {'nginx':
   ensure => 'present',
 }
-
-exec {'install_nginx_web_server':
-  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+->exec {'redirect_page':
+  command  => 'sudo sed -i "12i add_header X-Served-By \"${hostname}\";" /etc/nginx/sites-available/default',
   provider => shell,
 }
-
-exec {'set_hello_world_page':
-  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
-  provider => shell,
-}
-
-exec {'redirect_page':
-  command  => 'sudo sed -i "47i rewrite ^/redirect_me https://google.com permanent;" /etc/nginx/sites-available/default',
-  provider => shell,
-}
-
-exec {'run':
+->exec {'run':
   command  => 'sudo service nginx restart',
   provider => shell,
 }
