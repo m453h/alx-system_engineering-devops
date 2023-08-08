62bae43e3b0a19b9559ed53ea4113926f43ae7f9
Michael Hudson <mnkotagu@gmail.com>
2023-08-08 16:29:10 +0300

    Implement 0-subs solution

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This function queries the Reddit API and
+returns the number of subscribers
+(not active users, total subscribers)
+for a given subreddit
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the number of subscribers in a subreddit
+
+    Args:
+        subreddit (string): The name of the subreddit
+
+    Return:
+            number_of_subscribers (int)
+    """
+    if subreddit is None:
+        return 0
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+
+    try:
+        response = requests.get(url)
+        results = response.json()
+        return results.get('data').get('subscribers')
+    except Exception:
+        return 0
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,2 @@
+# 0x16. API advanced
+This folder contains the source for the API advanced Python Project. In this project we are using the Reddit API to make different calls.
