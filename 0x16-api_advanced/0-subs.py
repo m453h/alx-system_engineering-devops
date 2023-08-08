af19ab0bfed65c5e03c98511467f069500e77769
Michael Hudson <mnkotagu@gmail.com>
2023-08-08 16:50:36 +0300

    Add user agent to 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -18,13 +18,16 @@ def number_of_subscribers(subreddit):
     Return:
             number_of_subscribers (int)
     """
-    if subreddit is None or not isinstance(subreddit, str):
+    if subreddit is None:
         return 0
 
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    user_agent = {'User-agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.0)\
+     AppleWebKit/538.0.0 (KHTML, like Gecko) \
+     Chrome/17.0.821.0 Safari/538.0.0'}
 
     try:
-        response = requests.get(url)
+        response = requests.get(url, headers=user_agent)
         results = response.json()
         return results.get('data').get('subscribers')
     except Exception:
